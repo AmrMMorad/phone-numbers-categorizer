8764b0c0cbba89ec15f706362abed8ddfaa46e76
AmrMMorad <amr.morad@bibalex.org>
2021-11-09 20:54:16 +0200

    Initialize the phone numbers categorizer springboot project

---
+++ src/main/java/com/jumia/phonenumberscategorizer/phonenumberscategorizer/PhonenumberscategorizerApplication.java
@@ -0,0 +1,13 @@
+package com.jumia.phonenumberscategorizer.phonenumberscategorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonenumberscategorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonenumberscategorizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumia/phonenumberscategorizer/phonenumberscategorizer/PhonenumberscategorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.phonenumberscategorizer.phonenumberscategorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonenumberscategorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
